dc178c069ced0908eebb201d9b7a24c05ab48292
octskill <67458741+octskill@users.noreply.github.com>
2020-06-26 18:43:31 +0900

    9

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix = '-')
+
+@client.event
+async def on_ready():
+    
+    await client.change_presence(status=discord.Status.online)
+     
+    await client.change_presence(activity=discord.Game(name="게임 하는중"))
+    
+    print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+
+client.run(os.environ['token'])
+
+  
